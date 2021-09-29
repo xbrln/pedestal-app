aa28ce044590c7b8f0b2ac960cbea70c4e8fa742
jef <324976+xbrln@users.noreply.github.com>
2021-09-29 21:57:11 +0200

    Add params

--- src/hello.clj
+++ src/hello.clj
@@ -1,23 +1,74 @@
 (ns hello
-  (:require [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]))
+  (:require [clojure.data.json :as json]
+            [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http.content-negotiation :as conneg]))
+
+(def unmentionables #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin" "曹操"})
+
+(def supported-types ["text/html" "application/edn" "application/json" "text/plain"])
+
+(def content-neg-intc (conneg/negotiate-content supported-types))
+
+(defn ok [body]
+  {:status 200
+   :body body
+   :headers {"Content-Type" "text/html"}})
+
+(defn not-found []
+  {:status 404 :body "Not found\n"})
+
+(defn bad-request []
+  {:status 400 :body "Bad Request\n"})
+
+(defn greeting-for [nm]
+  (cond
+    (nil? nm) (ok "Hello, world!\n")
+    (empty? nm) (bad-request)
+    (unmentionables nm) (not-found)
+    :else (ok (str "Hello, " nm "\n"))))
 
 (defn respond-hello [request]
-  {:status 200 :body "test"})
+  (let [nm (get-in request [:query-params :name])
+        resp (greeting-for nm)]
+    resp))
+
+(def echo
+  {:name ::echo
+   :enter (fn [context]
+            (let [request (:request context)
+                  response (ok request)]
+              (assoc context :response response)))})
 
 (def routes
   (route/-expand-routes
-   #{["/greet" :get respond-hello :route-name :greet]}))
+   #{["/greet" :get [content-neg-intc respond-hello] :route-name :greet]
+     ["/echo" :get echo]}))
 
-(defn create-server []
-  (http/create-server
-   {::http/routes routes
-    ::http/type :jetty
-    ::http/port 8890}))
+(def service-map
+  {::http/routes routes
+   ::http/type :jetty
+   ::http/port 8890})
 
 (defn start []
-  (http/start (create-server)))
+  (http/start (http/create-server service-map)))
+
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server
+          (http/start
+           (http/create-server
+            (assoc service-map
+                   ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
 
 (comment
-  (respond-hello {})
-  (route/try-routing-for hello/routes :prefix-tree "/greet" :post))
+  (respond-hello {:query-params {:name "jef"}})
+  (restart))
